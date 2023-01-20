1b2ba3ce40a3ee5cadc33fdd79c5234634ee1907
suryathevar55 <88906447+suryathevar55@users.noreply.github.com>
2023-01-20 17:47:36 +0530

    Add files via upload

---
+++ Practical/Hello.java
@@ -0,0 +1,11 @@
+class My{
+public static void display(){
+System.out.println("Hi......");
+}
+}
+class Hello{
+public static void main(String[] args){
+System.out.println("MY Nth Program");
+My.display();
+}
+}
